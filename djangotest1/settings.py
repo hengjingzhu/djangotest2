f684cb289aea81790c5fa1a85debbbf0a8993cdd
davidzhu <david3311041@hotmail.com>
2023-02-28 22:12:53 +0800

    第四次提交

--- djangotest1/settings.py
+++ djangotest1/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=gh1hl5&twp%@7=ym$dt%21fod*gxr4=(j^fp=1_^kdb5k$yjn'
-
+#SECRET_KEY = 'django-insecure-=gh1hl5&twp%@7=ym$dt%21fod*gxr4=(j^fp=1_^kdb5k$yjn'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
